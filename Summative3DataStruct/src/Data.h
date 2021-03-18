a582d648f84c1a41e0b358d97e83b28ebd4df5fa
MG <mg-git.plumbic@8shield.net>
2021-03-18 09:41:20 +0800

    Fixed Comment Format

--- Summative3DataStruct/src/Data.h
+++ Summative3DataStruct/src/Data.h
@@ -7,18 +7,18 @@ class Graph
 {
 	private:
 	int V;
-	list <int> *adj;						           //Programmer2
-	int **adj2;								             //Programmer2
+	list <int> *adj;						 //Programmer2
+	int **adj2;								 //Programmer2
 	void DFSUtil (int v, bool visited []);	 //Lead
 	void BFSUtil (int s, bool visited []);	 //Lead
 
 	public:
-	Graph (int);								        //All	
-	void addEdge (int u, int v);				//All
-	void addEdge2 (int u, int v);			//Programmer2
-	void printGraph ();						    //All
-	void printGraph2 ();						    //Programmer2
-	void DFS (int v);						      //Lead
-	void BFS (int s);						      //Lead
+	Graph (int);						     //All	
+	void addEdge (int u, int v);		     //All
+	void addEdge2 (int u, int v);			 //Programmer2
+	void printGraph ();						 //All
+	void printGraph2 ();					 //Programmer2
+	void DFS (int v);						 //Lead
+	void BFS (int s);						 //Lead
 };
 
