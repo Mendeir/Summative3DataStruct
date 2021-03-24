3c174f5680e8bd318dd8e2c2c6f24c5e1a1bd19d
Dave Bornilla <T201911410@feucanvas.edu.ph>
2021-03-24 21:45:55 +0800

    main menu

--- Summative3DataStruct/src/Main.cpp
+++ Summative3DataStruct/src/Main.cpp
@@ -4,47 +4,98 @@ using namespace std;
 
 int main ()
 {
-	int ch;
-	Graph g (8);
-	cout << "Graph Operations" << endl;
-	cout << "[1] Adjacency List" << endl;
-	cout << "[2] Adjacency Matrix" << endl;
+	int choice;
+	
+	cout << "Create Graph [1] \n";
+	cout << "Traversal [2] \n";
+	cout << "Find Path [3] \n";
+	cout << "Path Cost [4] \n";
+	cout << "Exit [5] \n";
 	cout << "Enter choice: ";
-	cin >> ch;
-	if (ch == 1)
+	cin >> choice;
+	switch (choice)
 	{
-		g.addEdge (0, 6);
-		g.addEdge (1, 5);
-		g.addEdge (2, 0);
-		g.addEdge (2, 4);
-		g.addEdge (3, 5);
-		g.addEdge (4, 1);
-		g.addEdge (4, 3);
-		g.addEdge (5, 7);
-		g.addEdge (6, 1);
-		g.addEdge (6, 7);
-		g.printGraph ();
-		cout << endl << "DFS Traversal..." << endl;
-		g.DFS (1);
-		cout << endl << endl;
-		cout << endl << "BFS Traversal..." << endl;
-		g.BFS (3);
+	case 1:
+
+		int graphChoice;
+		cout << "* ******************************* *\n";
+		cout << "*           Create Graph          *\n";
+		cout << "* ******************************* *\n";
+		cout << "Graph Operations \n";
+		cout << "[1] Adjacency List \n";
+		cout << "[2] Adjacency Matrix \n";
+		cout << "Enter choice: ";
+		cin >> graphChoice;
+		switch (graphChoice)
+		{
+		case 1:
+			cout << "* ******************************* *\n";
+			cout << "*         Adjacency List          *\n";
+			cout << "* ******************************* *\n";
+			
+			break;
+		case 2:
+			
+			cout << "* ******************************* *\n";
+			cout << "*         Adjacency Matrix        *\n";
+			cout << "* ******************************* *\n";
+			break;
+		default:
+			cout << "Invalid Input \n";
+			break;
+		}
+		break;
+	case 2: 
+
+		int traversalChoice;
+		cout << "* ******************************* *\n";
+		cout << "*            Traversal            *\n";
+		cout << "* ******************************* *\n";
+		cout << "Graph Operations \n";
+		cout << "[1] BFS \n";
+		cout << "[2] DFS \n";
+		cout << "Enter choice: ";
+		cin >> traversalChoice;
+		switch (traversalChoice) {
+		case 1:
+			cout << "* ******************************* *\n";
+			cout << "*               BFS               *\n";
+			cout << "* ******************************* *\n";
+			break;
+		case 2:
+			cout << "* ******************************* *\n";
+			cout << "*               DFS               *\n";
+			cout << "* ******************************* *\n";
+			break;
+		default :
+			cout << "Invalid Input \n";
+			break;
+		}
+		break;
+	case 3:
+		cout << "* ******************************* *\n";
+		cout << "*           Find Path             *\n";
+		cout << "* ******************************* *\n";
+		break;
+
+	case 4:
+		cout << "* ******************************* *\n";
+		cout << "*            Path Cost            *\n";
+		cout << "* ******************************* *\n";
+		break;
+
+	case 5:
+		cout << "* ******************************* *\n";
+		cout << "*          Exit Program           *\n";
+		cout << "* ******************************* *\n";
+		break;
+
+	default:
+		cout << "Invalid Input \n";
+		break;
 	}
-	else if (ch == 2)
-	{
-		g.addEdge2 (0, 6);
-		g.addEdge2 (1, 5);
-		g.addEdge2 (2, 0);
-		g.addEdge2 (2, 4);
-		g.addEdge2 (3, 5);
-		g.addEdge2 (4, 1);
-		g.addEdge2 (4, 3);
-		g.addEdge2 (5, 7);
-		g.addEdge2 (6, 1);
-		g.addEdge2 (6, 7);
-		g.printGraph2 ();
-	}
-	cout << endl << endl;
+	
+	
 }
 
 
