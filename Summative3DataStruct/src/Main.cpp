7d56d126e51ee6853fe84117780e981e3b55ed69
MG <mg-git.plumbic@8shield.net>
2021-03-26 21:11:27 +0800

    Updated

--- Summative3DataStruct/src/Main.cpp
+++ Summative3DataStruct/src/Main.cpp
@@ -1,103 +1,127 @@
 #include <iostream>
 #include "Data.h"
 using namespace std;
-void clear() 
-{ 
-	cout << "\033[H\033[J"; 
+void clear ()
+{
+	cout << "\033[H\033[J";
 }
 // Author : Bornilla, Dave Y.
-int main()
+int main ()
 {
-	Graph countries(7);
+	Graph countries (7);
 	int choice;
-	while (true) {
-	cout << "[1] Create Graph \n";
-	cout << "[2] Traversal \n";
-	cout << "[3] Exit \n\n";
-	cout << "Enter choice: ";
-	cin >> choice;
-	
+	while (true)
+	{
+		cout << "[1] Create Graph \n";
+		cout << "[2] Traversal \n";
+		cout << "[3] Exit \n\n";
+		cout << "Enter choice: ";
+		cin >> choice;
+
 		switch (choice)
 		{
 
-		case 1:
-			clear();
-			int graphChoice;
-			cout << "* ******************************* *\n";
-			cout << "*           Create Graph          *\n";
-			cout << "* ******************************* *\n";
-			cout << "Graph Operations \n";
-			cout << "[1] Adjacency List \n";
-			cout << "[2] Adjacency Matrix \n";
-			cout << "Enter choice: ";
-			cin >> graphChoice;
-			switch (graphChoice)
-			{
 			case 1:
-				clear();
+				clear ();
+				int graphChoice;
 				cout << "* ******************************* *\n";
-				cout << "*         Adjacency List          *\n";
+				cout << "*           Create Graph          *\n";
 				cout << "* ******************************* *\n";
-				countries.populateList();
-				countries.printGraph();
+				cout << "Graph Operations \n";
+				cout << "[1] Adjacency List \n";
+				cout << "[2] Adjacency Matrix \n";
+				cout << "Enter choice: ";
+				cin >> graphChoice;
+				switch (graphChoice)
+				{
+					case 1:
+						clear ();
+						cout << "* ******************************* *\n";
+						cout << "*         Adjacency List          *\n";
+						cout << "* ******************************* *\n";
+						countries.populateGraph ();
+						countries.printGraph ();
+						cin.get ();
+						break;
+					case 2:
+						clear ();
+						cout << "* ******************************* *\n";
+						cout << "*         Adjacency Matrix        *\n";
+						cout << "* ******************************* *\n";
+						countries.populateMatrix ();
+						countries.printGraph2 ();
+						cin.get ();
+						break;
+					default:
+						cout << "Invalid Input \n";
+						break;
+				}
+				cout << "Enter any key to continue..";
+				cin.get ();
+				clear ();
 				break;
 			case 2:
-				clear();
+				clear ();
+				int traversalChoice;
 				cout << "* ******************************* *\n";
-				cout << "*         Adjacency Matrix        *\n";
+				cout << "*            Traversal            *\n";
 				cout << "* ******************************* *\n";
-				countries.populateMatrix();
-				countries.printGraph2();
+				cout << "Graph Operations \n";
+				cout << "[1] BFS \n";
+				cout << "[2] DFS \n";
+				cout << "Enter choice: ";
+				cin >> traversalChoice;
+				switch (traversalChoice) {
+					case 1:
+						clear ();
+						int userCountry;
+						cout << "* ******************************* *\n";
+						cout << "*               BFS               *\n";
+						cout << "* ******************************* *\n";
+						cout << "Countries \n";
+						cout << "[0] Atlanta \n";
+						cout << "[1] Austin \n";
+						cout << "[2] Chicago \n";
+						cout << "[3] Dallas \n";
+						cout << "[4] Denver \n";
+						cout << "[5] Houston \n";
+						cout << "[6] Washington \n";
+						cout << "Select a country: \n";
+						cin >> userCountry;
+						break;
+
+					case 2:
+						clear ();
+						int userCountry1;
+						cout << "* ******************************* *\n";
+						cout << "*               DFS               *\n";
+						cout << "* ******************************* *\n";
+						cout << "Countries \n";
+						cout << "[0] Atlanta \n";
+						cout << "[1] Austin \n";
+						cout << "[2] Chicago \n";
+						cout << "[3] Dallas \n";
+						cout << "[4] Denver \n";
+						cout << "[5] Houston \n";
+						cout << "[6] Washington \n";
+						cout << "Select a country: \n";
+						cin >> userCountry;
+						break;
+					default:
+						cout << "Invalid Input \n";
+						break;
+				}
 				break;
+			case 3:
+				cout << "* ******************************* *\n";
+				cout << "*          Exit Program           *\n";
+				cout << "* ******************************* *\n";
+				break;
+
 			default:
 				cout << "Invalid Input \n";
 				break;
-			}
-			break;
-		case 2:
-			clear();
-			int traversalChoice;
-			cout << "* ******************************* *\n";
-			cout << "*            Traversal            *\n";
-			cout << "* ******************************* *\n";
-			cout << "Graph Operations \n";
-			cout << "[1] BFS \n";
-			cout << "[2] DFS \n";
-			cout << "Enter choice: ";
-			cin >> traversalChoice;
-			switch (traversalChoice) {
-			case 1:
-				clear();
-				cout << "* ******************************* *\n";
-				cout << "*               BFS               *\n";
-				cout << "* ******************************* *\n";
-				countries.BFS (0);
-				break;
-				
-			case 2:
-				clear();
-				cout << "* ******************************* *\n";
-				cout << "*               DFS               *\n";
-				cout << "* ******************************* *\n";
-				countries.DFS (3);
-				break;
-			default:
-				cout << "Invalid Input \n";
-				break;
-			}
-			break;
-		case 3:
-			cout << "* ******************************* *\n";
-			cout << "*          Exit Program           *\n";
-			cout << "* ******************************* *\n";
-			break;
-		
-		default:
-			cout << "Invalid Input \n";
-			break;
 		}
-		
 	}
+
 }
-
-
